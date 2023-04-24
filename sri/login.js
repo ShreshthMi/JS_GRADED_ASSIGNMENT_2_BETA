ebc77cc1933da879b0fe0dbb1d7a8c961c14ac4f
ShreshthMi <113287087+ShreshthMi@users.noreply.github.com>
2023-04-24 16:01:42 +0530

    SRI CORRECTIONS

--- sri/login.js
+++ sri/login.js
@@ -1,24 +1,24 @@
 function validateData() {
     var uid = document.getElementById("username").value;
     var pw = document.getElementById("password").value;
+  
     var getusername = localStorage.getItem("uid");
     var getpassword = localStorage.getItem("pwd");
-
-    if (uid == getusername) {
-        if (pw == getpassword) {
-            alert("login successful");
-            // <script>
-            // function myFunction() {
-            //     location.replace("Resumepage.js")
-            //   }
-            //   </script>
-            //redirect to Resume page
-            //         If successful, direct the user to Resume Page
-            // 4.	Once the user is in the Resume page restrict the user from going back to the login page. (When clicked on the back button in the browser, restrict the user from going back to the login page).
-
-        }
+  
+    if (uid === getusername && pw ===getpassword) {
+      
+      alert("login successful");
+        
+      // redirect to Resume page
+      window.location.replace("resume.html");
+      
+      // restrict user from going back to login page
+      history.pushState(null, null, location.href);
+      window.onpopstate = function () {
+        history.go(1);
+      };
+    } else {
+      alert("invalid username/password");
     }
-    else {
-        alert("invalid username/password");
-    }
-}
+  }
+  
--- sri/storeData.js
+++
@@ -1,6 +0,0 @@
-function storeData() {
-    var uid = document.getElementById("username").value;
-    var pw = document.getElementById("password").value;
-    localStorage.setItem("uid", uid);
-    localStorage.setItem("pw", pw);
-}
